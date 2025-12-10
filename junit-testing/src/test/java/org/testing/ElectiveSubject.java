a21bf0096f0288e945bf3095d2db9a09a5adb9fd
LennoxOwusuAfriyie <lennox.afriyie@amalitech>
2025-12-10 08:24:56 +0000

    addition of test logic

--- junit-testing/src/test/java/org/testing/ElectiveSubject.java
+++ junit-testing/src/test/java/org/testing/ElectiveSubject.java
@@ -1,4 +1,25 @@
+
 package org.testing;
 
-public class ElectiveSubject {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ElectiveSubjectTest {
+
+    @Test
+    void constructsElectiveSubject_withNameAndCode() {
+        ElectiveSubject art = new ElectiveSubject("Visual Arts", "ART201");
+        assertEquals("Visual Arts", art.getSubjectName());
+        assertEquals("ART201", art.getSubjectCode());
+    }
+
+    @Test
+    void rejectsNullOrBlankValues_ifValidatedInConstructor() {
+        // Keep if you validate inputs; otherwise remove.
+        assertThrows(IllegalArgumentException.class, () -> new ElectiveSubject(null, "ART201"));
+        assertThrows(IllegalArgumentException.class, () -> new ElectiveSubject("Visual Arts", null));
+        assertThrows(IllegalArgumentException.class, () -> new ElectiveSubject("", "ART201"));
+        assertThrows(IllegalArgumentException.class, () -> new ElectiveSubject("Visual Arts", "   "));
+    }
 }
+
