c88186669c1a919b9ad2648972335b4ea36047f6
LennoxOwusuAfriyie <lennox.afriyie@amalitech>
2025-12-10 06:34:49 +0000

    adding of RegularStudent class for testing

---
+++ junit-testing/src/main/java/org/testing/RegularStudent.java
@@ -0,0 +1,20 @@
+package org.testing;
+
+//Represents a regular student with standard passing grade logic.
+public class RegularStudent extends Student {
+
+    //Constructs a regular student with personal details.
+    public RegularStudent(String name, int age, String email, String phone)
+    {
+        super(name, age, email);
+    }
+
+
+    @Override
+    public String getStudentType() { return "Regular Student"; }
+
+    @Override
+    public double getPassingGrade() {
+        return 50.0; }
+}
+
